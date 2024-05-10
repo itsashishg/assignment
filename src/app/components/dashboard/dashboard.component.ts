129e9b892a62a68e3ff087ea4da7fa81be267b42
itsashishg <itsashishg@duck.com>
2024-05-11 02:43:34 +0530

    Basic dashboard implemented.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,33 +1,57 @@
+import { OnInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
 import { PinData } from '../models';
 import { DataService } from '../../services/data.service';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatSortModule, MatPaginatorModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.less'
 })
 export class DashboardComponent implements OnInit {
-  data: PinData[] = []
+  // data: [] = []
+  displayedColumns: string[] = ['title', 'imageURL', 'collaborators', 'privacy'];
+  dataSource: MatTableDataSource<PinData> = new MatTableDataSource<PinData>();
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   constructor(private service: DataService) { }
 
   ngOnInit() {
     this.fetchData();
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   fetchData() {
     this.service.getTableData().subscribe({
       next: (res) => {
-        this.data = res;
+        this.dataSource.data = res;
       },
       error: (err) => {
         console.log(err);
       }
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
